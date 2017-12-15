ce4cc48e6b9dbc092103f76358815d2e2dd6c1a1
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-12-14 20:13:19 -0700

    ./addCommitPush.sh

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Header } from 'react-native-elements'
 import Button from 'react-native-button'
 
 import {createStore, applyMiddleware} from 'redux'
 import {Provider, connect} from 'react-redux'
 import PropTypes from 'prop-types'
 
+const textStyle = {
+  fontSize: 30,
+  textAlign: 'center',
+}
+
 const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  text: textStyle,
+  header: {
+    paddingTop: 100,
+    width: '100%',
+    text: {
+      ...textStyle,
+      color: '#fff',
+    }
+  }
 }
 
 export default () =>
-  <View style={styles.container}>
-    <Text>You are a potato</Text>
+  <View>
+  <Header style={styles.header}
+    centerComponent={{
+      text: 'Trivia Challenge',
+      style: styles.header.text
+    }}/>
+    <View style={styles.container}>
+      <Text
+        style={{...styles.text, fontWeight: 'bold'}}>
+          Welcome To The{"\n"}Trivia Challenge!
+        </Text>
+      <Text style={styles.text}>
+        You Will be presented with 10 True or False Questions.
+      </Text>
+      <Text style={styles.text}>Can you score 100%?</Text>
+      <Button style={styles.text}>BEGIN</Button>
+    </View>
   </View>
