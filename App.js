90c535f4dd5153ec5a8cb4e90969eb55d7f2ac7a
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-12-14 18:53:23 -0700

    ./addCommitPush.sh

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Button from 'react-native-button'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import {createStore, applyMiddleware} from 'redux'
+import {Provider, connect} from 'react-redux'
+import PropTypes from 'prop-types'
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}
+
+export default () =>
+  <View style={styles.container}>
+    <Text>You are a potato</Text>
+  </View>
