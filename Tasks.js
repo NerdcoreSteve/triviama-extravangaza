2a978b36bfc65d349b972aca60a107c1ac6a597e
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-12-21 00:19:26 -0700

    ./addCommitPush.sh

--- Tasks.js
+++ Tasks.js
@@ -1,19 +1,26 @@
 import R from 'ramda'
+import he from 'he'
+
 import {futurizeP} from 'futurize'
 import Task from 'data.task'
-
 const futurizePromise = futurizeP(Task)
 const fetchTask = futurizePromise(fetch)
 
 const getQuestions = () =>
   fetchTask('https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean')
-    .map(R.prop('_bodyText'))
-    .map(JSON.parse)
-    .chain(results =>
-      results.response_code === 0
-        ? Task.of(results)
-        : Task.rejected(results))
-    .map(({results: questions}) => ({type: 'GOT_QUESTIONS', questions}))
+    .map(R.pipe(R.prop('_bodyText'), JSON.parse))
+    .chain(response =>
+      response.response_code === 0
+        ? Task.of(response.results)
+        : Task.rejected(response.results))
+    .map(
+      R.map(
+        R.mapObjIndexed(
+          (value, key) =>
+            key === 'category' || key === 'correct_answer' || key === 'question'
+              ? he.decode(value)
+              : value)))
+    .map(questions => ({type: 'GOT_QUESTIONS', questions}))
     .rejectedMap(() => ({type: 'CHANGE_SCREEN', screen: 'noQuestions'}))
 
 export {getQuestions}
